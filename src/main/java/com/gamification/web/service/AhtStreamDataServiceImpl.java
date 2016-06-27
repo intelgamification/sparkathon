3aaf5c9472cf3c41160ac4379af3bb1c6eca5a5b
Pranit Dhoke <pranit.dhoke@nice.com>
2016-06-28 03:52:41 +0530

    Stream Controller changes

--- src/main/java/com/gamification/web/service/AhtStreamDataServiceImpl.java
+++ src/main/java/com/gamification/web/service/AhtStreamDataServiceImpl.java
@@ -3,6 +3,8 @@ package com.gamification.web.service;
 import org.dozer.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.gamification.web.dao.AhtStreamDataDAO;
 import com.gamification.web.model.AhtStreamData;
@@ -16,6 +18,7 @@ public class AhtStreamDataServiceImpl implements AhtStreamDataService {
 	@Autowired
 	private Mapper dozerMapper;
 	
+	@Transactional(propagation=Propagation.REQUIRES_NEW)
 	public AhtStreamData getAhtStream() {
 		com.gamification.web.entity.AhtStreamData ahtStreamData=ahtStreamDataDAO.getAhtStream();
 		AhtStreamData data = dozerMapper.map(ahtStreamData, AhtStreamData.class);
